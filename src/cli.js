503709cc5cc59969a9df98df8614aa63d1e514f5
evgeniy1801 <evgeniy1801@gmail.com>
2020-12-15 19:59:58 +0700

    Step 3: add welcome function

---
+++ src/cli.js
@@ -0,0 +1,9 @@
+import readLineSync from 'readline-sync';
+
+function welcomeUser() {
+  const userName = readLineSync.question('Как вас зовут? ');
+
+  console.log(`Привет, ${userName}!`);
+}
+
+export { welcomeUser };
